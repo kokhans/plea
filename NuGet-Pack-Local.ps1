aaddfe310e3596575a1c33253df9e0a891f23a65
Serhii Kokhan <10929988+kokhans@users.noreply.github.com>
2022-08-23 13:20:00 +0300

    Add NuGet-Pack-Local.ps1

---
+++ NuGet-Pack-Local.ps1
@@ -0,0 +1,6 @@
+﻿Set-Variable output -option Constant -value "nupkgs"
+Set-Variable source -option Constant -value "C:\NuGet\packages"
+
+# Core
+Invoke-Expression "dotnet pack .\Sources\Plea.Core\Plea.Core.csproj -p:PackageVersion=$($args[0]).$($args[1]).$($args[2])$($args[3]) --version-suffix $($args[3]) --output $output"
+Invoke-Expression "nuget add .\nupkgs\Plea.Core.$($args[0]).$($args[1]).$($args[2])$($args[3]).nupkg -source $source"
